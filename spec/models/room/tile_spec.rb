826b8b45a7b059a2e61c361d6e657319104d633e
Jon Daniel <binarycleric@gmail.com>
2016-10-16 16:05:30 -0400

    Renaming to tile

--- spec/models/room/tile_spec.rb
+++ spec/models/room/tile_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
-RSpec.describe Room::Table do
+RSpec.describe Room::Tile do
 
 
   it 'xxx' do
     row = Room::Row.new
 
     3.times do
-      table = Room::Table.new length: 8, width: 2
-      row.add_table(table.clone)
+      tile = Room::Tile.new length: 8, width: 2
+      row.add_tile(tile.clone)
     end
 
     expect(row.length).to eql (3 * 8)
@@ -18,8 +18,8 @@ RSpec.describe Room::Table do
     row = Room::Row.new
 
     3.times do
-      table = Room::Table.new length: 8, width: 2
-      row.add_table(table.clone)
+      tile = Room::Tile.new length: 8, width: 2
+      row.add_tile(tile.clone)
     end
 
     expect(row.width).to eql 2
@@ -28,8 +28,8 @@ RSpec.describe Room::Table do
   it 'returns false when not full' do
     row = Room::Row.new(max_length: 24)
     2.times do
-      table = Room::Table.new length: 8, width: 2
-      row.add_table(table.clone)
+      tile = Room::Tile.new length: 8, width: 2
+      row.add_tile(tile.clone)
     end
 
     expect(row.full?).to be_falsey
@@ -38,8 +38,8 @@ RSpec.describe Room::Table do
   it 'returns true when full' do
     row = Room::Row.new(max_length: 16)
     2.times do
-      table = Room::Table.new length: 8, width: 2
-      row.add_table(table.clone)
+      tile = Room::Tile.new length: 8, width: 2
+      row.add_tile(tile.clone)
     end
 
     expect(row.full?).to be_truthy
@@ -49,18 +49,18 @@ RSpec.describe Room::Table do
     let(:row) { Room::Row.new(max_length: 16) }
 
     it 'returns true if row has enough room' do
-      table = Room::Table.new length: 8, width: 2
-      expect(row.enough_room?(table)).to be_truthy
+      tile = Room::Tile.new length: 8, width: 2
+      expect(row.enough_room?(tile)).to be_truthy
     end
 
     it 'returns false is row is full' do
       2.times do
-        table = Room::Table.new length: 8, width: 2
-        row.add_table(table.clone)
+        tile = Room::Tile.new length: 8, width: 2
+        row.add_tile(tile.clone)
       end
 
-      table = Room::Table.new length: 8, width: 2
-      expect(row.enough_room?(table)).to be_falsey
+      tile = Room::Tile.new length: 8, width: 2
+      expect(row.enough_room?(tile)).to be_falsey
     end
   end
 
