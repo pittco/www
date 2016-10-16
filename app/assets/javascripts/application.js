117491ce4f8c8a4b4af84bfb3b14441cdb9d23ec
Jon Daniel <binarycleric@gmail.com>
2016-10-16 18:57:39 -0400

    Adding basic seating selection

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,3 +2,18 @@
 //= require bootstrap-sprockets
 //= require jquery_ujs
 //= require_tree .
+
+
+$(document).ready(function() {
+  var $seating_chart = $("#seating-chart");
+  var $seats = $seating_chart.find("td.seat-cell");
+  var $empty_spaces = $seating_chart.find("td.empty-space");
+
+  $seats.click(function() {
+    $(this).toggleClass("seat-chosen");
+  });
+
+  $empty_spaces.each(function(k, element) {
+  
+  });
+});
